efd1f2d8dac95ac9ee04feeb1702f844d8866ff2
Jacob Ashworth <jeashwor@gmail.com>
2020-06-27 21:44:52 -0400

    updated ReadMe file with project description and screen shots

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Random-Password-Generator
-Input parameters such as number of characters and character types to generate a random password. 
+Deployed Application: [Random Password Generator](https://jeashwor.github.io/Random-Password-Generator/)
+
+## Criteria
+- Prompt Length of password between 8 - 128.
+- Confirm use of lower case, upper case, numbers, and special characters. 
+- Validate that at least one set of above characters has been chosen. 
+- Generate Password and display in corresponding area of provided HTML document.
+
+## Functionality created
+- Added Event Listener to button element. 
+- Created object to contain character sets to be used. 
+- Created function to accomplish the following
+    - Take user inputs.
+    - Build character set from user inputs
+    - Produce random password using Math.random within a for loop to build the password from defined character set.
+    - Return password value to be passed into HTML document.  
+
+## JavaScript Screen Shot
+![JavascriptScreenShot](./jsScreenShot.png)
+
+## HTML Page with password result for 50 characters using lower case, upper case, numbers, and special characters. 
+![HTMLScreenShot](./htmlScreenShot.png)
--- script.js
+++ script.js
@@ -5,31 +5,31 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Created object to draw possible characters from
 var characters = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
   symbols: "<>`~!@#$%^&*()-_=+[]{}|\"\\\r\n"
 }
-userPassword = "";
 
 
-function generatePassword(lower, upper, number, symbol, length) {
+// Created functino called generatePassword
+function generatePassword() {
   var userPasswordCharacters = "";
+  var userPassword = "";
   var length = prompt("Enter the desired number of password characters.  (Must be a number between 8 and 128)");
   var useLower = confirm("Do you want to include lower case letters?");
   var useUpper = confirm("Do you want to include Upper case letters?");
   var useNumber = confirm("Do you want to include numbers?");
   var useSymbol = confirm("Do you want to include Special Characters?");
-  
+
   if (useNumber === true) {
     userPasswordCharacters = userPasswordCharacters + characters.number;
   }
@@ -53,33 +53,3 @@ function generatePassword(lower, upper, number, symbol, length) {
   console.log(userPassword);
 }
 
-
-
-// Code bits from tutorials
-
-
-// const randomFunc = {
-//   lower: getRndLower,
-//   upper: getRndUpper,
-//   number: getRndNumber,
-//   symbol: getRndSymbol
-// };
-
-
-// function getRndLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRndUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function getRndNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function getRndSymbol() {
-//   const symbols = " !#$%&()*+,-./:;<=>?@[\]^_`{|}~"
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-// console.log(getRndSymbol());
